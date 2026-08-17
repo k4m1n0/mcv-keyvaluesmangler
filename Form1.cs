7797fb6d3eae26037677317bf42d68727428a160
k0u74ku <k4m1n0@outlook.com>
2026-08-17 19:40:02 +0800

    bruh

--- Form1.cs
+++ Form1.cs
@@ -481,7 +481,7 @@ public partial class Form1 : Form
 
         var wTemp = new WeaponData();
         SaveControlsToWeapon(wTemp, bLeftToRight);
-        var wDstCur = new WeaponData();
+        var wDstCurr = new WeaponData();
         SaveControlsToWeapon(wDst, !bLeftToRight);
         if (!WeaponDataEquals(wTemp, wDst))
         {
